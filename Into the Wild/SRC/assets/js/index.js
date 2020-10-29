7e95d21b53767663eb81e0336f83f2d5c5dcc5c9
T-N9 <tenyainmoelwin@ucsh.edu.mm>
2020-10-29 17:42:21 +0630

    intoTheWild beta

---
+++ Into the Wild/SRC/assets/js/index.js
@@ -0,0 +1,27 @@
+new WOW().init();
+$(".casts_slide").slick({
+    arrows:false,
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        }
+    ]
+});
