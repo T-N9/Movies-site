947a68be99d98f8bd61c351c397a6b24ac18b5a4
T-N9 <tenyainmoelwin@ucsh.edu.mm>
2020-10-30 16:08:02 +0630

    intoTheWild Beta 1.1

--- Into the Wild/SRC/assets/js/index.js
+++ Into the Wild/SRC/assets/js/index.js
@@ -1,3 +1,61 @@
+let screenHeight=$(window).height();
+console.log(screenHeight);
+
+$('.nav-link').click(function(){
+    $('.navbar-collapse').removeClass('show');
+    $('.menu-icon').addClass('fa-chevron-down');
+    
+});
+
+$(window).scroll(function () {
+    let currentPos=$(this).scrollTop();
+
+    if(currentPos>=screenHeight-400){
+        $('.m-header').addClass('m-navbar-scroll');
+    }
+    else{
+        $('.m-header').removeClass('m-navbar-scroll');
+        navActive('home');
+        // $("#up-link").removeClass('d-block');
+        // $("#up-link").addClass('d-none');
+    }
+});
+$('.navbar-toggler').click(function () {
+    let result=$('.navbar-collapse').hasClass('show');
+    console.log(result);
+
+    if(result){
+        $('.menu-icon').removeClass('fa-chevron-up').addClass('fa-chevron-down');
+    }
+    else{
+        $('.menu-icon').removeClass('fa-chevron-down').addClass('fa-chevron-up');
+        
+    }
+});
+
+function navActive(current){
+    $(`.nav-link`).removeClass("nav-active");
+    $(`.nav-link[href='#${current}']`).addClass("nav-active");
+}
+function navScroll(){
+    let currentSec=$("section[id]");
+    currentSec.waypoint(function(direction){
+        if(direction=="down"){
+            let currentId=$(this.element).attr('id');
+            console.log(currentId);
+            navActive(currentId);
+        }
+    },{offset:'0px'});
+    currentSec.waypoint(function(direction){
+        if(direction=="up"){
+            let currentId=$(this.element).attr('id');
+            console.log(currentId);
+            navActive(currentId);
+        }
+    },{offset:'-10px'});
+}
+
+navScroll();
 new WOW().init();
 $(".casts_slide").slick({
     arrows:false,
